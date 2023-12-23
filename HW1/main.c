ab49279967089157be5a21a67817c8ac6bf9589d
wer590 <wer59232@gmail.com>
2023-12-23 13:24:36 +0700

    first commit

---
+++ HW1/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int main(void)
+{
+	printf("Hello world!\n");
+	printf("Let's\ngo\n\t\tto walk\n");
+	printf("   *\n  ***\n *****\n*******\n");
+	printf(" HH HH\n ZZZZZ\n");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# 02.-Programming-in-C.-Basic-level
