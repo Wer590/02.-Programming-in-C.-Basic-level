1e904edaf3a19a59eece3505f6fa1de910e37600
wer590 <wer59232@gmail.com>
2023-12-30 19:15:09 +0700

    new file:   HW3/task#1.c 	new file:   HW3/task#2.c 	new file:   HW3/task#3.c 	new file:   HW3/task#4.c 	new file:   HW3/task#5.c

--- HW1/main.c
+++ HW1/main.c
@@ -1,9 +1,18 @@
+/*
+   Домашнее задание по лексии №1
+*/
+
 #include "stdio.h"
 
 int main(void)
 {
+	//Вывести на экран текст "Hello world!"
 	printf("Hello world!\n");
+	
+	//Вывести на экран текст лесенкой
 	printf("Let's\ngo\n\t\tto walk\n");
+	
+	//Вывести на экран рисунок из букв
 	printf("   *\n  ***\n *****\n*******\n");
 	printf(" HH HH\n ZZZZZ\n");
 	return 0;
