15fbfac913e7dd127c52be99cc2e5bfbc582ef1c
wer590 <wer59232@gmail.com>
2024-01-16 22:22:54 +0700

    modified:   HW6/FSM.c

--- HW6/C03.c
+++ HW6/C03.c
@@ -24,14 +24,14 @@
 
 int middle(int a, int b)
 {
-	return (a + b) / 2;
+    return (a + b) / 2;
 }
 
 int main(int argc, char **argv)
 {
-	int nunber_a, nunber_b;	
-	scanf("%d %d", &nunber_a, &nunber_b);
-	printf("%d\n", middle(nunber_a, nunber_b));	
-	return 0;
+    int nunber_a, nunber_b;	
+    scanf("%d %d", &nunber_a, &nunber_b);
+    printf("%d\n", middle(nunber_a, nunber_b));	
+    return 0;
 }
 
--- HW6/FSM.c
+++ HW6/FSM.c
@@ -1,108 +1,89 @@
 /*
  * FSM.c
- * 
+ *
  *	Машина состояний (FSM) и конечные автоматы
  *
- * 
- * 
+ *
+ *
  */
 
 
 #include <stdio.h>
 #include <conio.h>
 
-int GetUserSignal(void)
+
+enum states { READY, PREPFRE, WAIT, CHANGE, RETURN };
+enum signals { ONE_RUBL, TWO_RUBLS, CANCEL, NONE };
+
+enum signals GetUserSignal(void)
 {
-	static int flag = 0;
-	char Choice;    
-	while(1)
-	{
-		if (!flag)
-		{
-			printf("1.Put 1 rubl\n2.Put 2 rubl\n0.Cancel\n");
-			flag = 1;
-		}
-		Choice = getch();
-		switch(Choice)
-		{
-			case '1':
-				flag = 0;
-				return 0;
-			case '2': 
-				flag = 0;
-				return 1;
-			case '0':
-				flag = 0;
-				return 2;
-		}
-	}  
+    char Choice;
+    while(1)
+    {
+        printf("1.Put 1 rubl\n2.Put 2 rubl\n0.Cancel\n");
+        Choice = getch();
+        switch(Choice)
+        {
+        case '1':
+            return ONE_RUBL;
+        case '2':
+            return TWO_RUBLS;
+        case '0':
+            return CANCEL;
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
-	enum states 
-	{ 
-		READY	= 0,
-		PREPFRE = 1, 
-		WAIT	= 2, 
-		CHANGE	= 3, 
-		RETURN	= 4
-	} 
-	state = READY;
-	enum user_signal_states 
-	{
-		RUBL_1 = 0,
-		RUBL_2 = 1,
-		CANCEL = 2
-	}
-	user_signal = CANCEL;
-	while (1)
-	{
-		switch (state) 
-		{
-			case READY:
-				printf("Ready\n");
-				user_signal = GetUserSignal();        
-				if(user_signal == RUBL_2)
-				{
-					state = PREPFRE;
-				}
-				if(user_signal == RUBL_1)
-				{
-					state = WAIT;
-				}
-			break;   
-			case PREPFRE:
-				printf("Preapare cofee\n");
-				state = READY;      
-			break;  
-			case WAIT:
-				printf("Wait\n");
-				user_signal = GetUserSignal();    
-				if(user_signal == RUBL_2)
-				{
-					state = CHANGE;
-				}
-				if(user_signal == RUBL_1)
-				{
-					state = PREPFRE;
-				}
-				if(user_signal == CANCEL)
-				{
-					state = RETURN;
-				}    
-			break;        
-			case CHANGE:
-				printf("Change 1 Rubl\n");  
-				state = PREPFRE;    
-				break; 
-			case RETURN:
-				printf("Change 1 Rubl\n");  
-				state = READY;     
-			break;       
-		}
-	}
-	return 0;
+    enum states state = READY;
+    while (1)
+    {
+        switch (state)
+        {
+        case READY:
+            printf("Ready\n");
+            enum signals user_signal = GetUserSignal();
+            if(user_signal == TWO_RUBLS)
+            {
+                state = PREPFRE;
+            }
+            if(user_signal == ONE_RUBL)
+            {
+                state = WAIT;
+            }
+            break;
+        case PREPFRE:
+            printf("Preapare cofee\n");
+            state = READY;
+            break;
+        case WAIT:
+            printf("Wait\n");
+            user_signal = GetUserSignal();
+            if(user_signal == TWO_RUBLS)
+            {
+                state = CHANGE;
+            }
+            if(user_signal == ONE_RUBL)
+            {
+                state = PREPFRE;
+            }
+            if(user_signal == CANCEL)
+            {
+                state = RETURN;
+            }
+            break;
+        case CHANGE:
+            printf("Change 1 Rubl\n");
+            state = PREPFRE;
+            break;
+        case RETURN:
+            printf("Change 1 Rubl\n");
+            state = READY;
+            break;
+        }
+    }
+    return 0;
 }
 
 
